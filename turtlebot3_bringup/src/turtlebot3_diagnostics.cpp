9f9c2a4ba389780f36a477c0127db48fe2658dc6
Gilbert <kkjong89@naver.com>
2018-01-30 11:04:11 +0900

    add

--- turtlebot3_bringup/src/turtlebot3_diagnostics.cpp
+++ turtlebot3_bringup/src/turtlebot3_diagnostics.cpp
@@ -26,7 +26,7 @@
 #include <turtlebot3_msgs/VersionInfo.h>
 
 #define SOFTWARE_VERSION "1.0.0"
-#define FIRMWARE_VERSION "1.0.16"
+#define FIRMWARE_VERSION "1.0.15"
 #define HARDWARE_VERSION "1.0.0"
 
 ros::Publisher tb3_diagnostics_pub;
@@ -106,11 +106,9 @@ void versionMsgCallback(const turtlebot3_msgs::VersionInfo::ConstPtr &msg)
 
   if (std::string(msg->software) != std::string(SOFTWARE_VERSION))
     ROS_WARN("Check turtlebot3 repository and Update your software!!");
-  
-  if (std::string(msg->hardware) != std::string(HARDWARE_VERSION))
+  else if (std::string(msg->hardware) != std::string(HARDWARE_VERSION))
     ROS_WARN("Check turtlebot3 wiki page and Update your hardware!!");
-    
-  if (std::string(msg->firmware) != std::string(FIRMWARE_VERSION))
+  else if (std::string(msg->firmware) != std::string(FIRMWARE_VERSION))
     ROS_WARN("Check OpenCR update and change your firmware!!");
 }
 
