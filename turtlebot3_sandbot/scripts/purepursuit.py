a8084d2deeb718c97d6be99c6c3d801e2f639208
Mingg8 <mingg9688@gmail.com>
2018-04-10 20:20:42 +0900

    Add new alphabets

--- turtlebot3_sandbot/scripts/purepursuit.py
+++ turtlebot3_sandbot/scripts/purepursuit.py
@@ -38,19 +38,42 @@ If you want to close, insert 's'
 
 increment = 5
 
-arr_path_B = []
-with open(sys.argv[1], "r") as file_path_B:
-    for idx, line in enumerate(file_path_B):
-        str = line.split()
-        if not (len(str)==0):
-            #print(str)
-            arr_path_B.append([(float)(str[1]), (float)(str[0])])
-        else:
-            break
+def get_path(word):
+    print("b")
+    arr_path=[]
+    dir_1= "/home/mjlee/catkin_ws/src/turtlebot3/turtlebot3_sandbot/data_path/path_"
+    dir_2=".txt"
+    i=0.0
+    print("c")
+    for letter in list(word):
+        with open(dir_1+letter.capitalize()+dir_2,"r") as file_path:
+            for idx, line in enumerate(file_path):
+                print("letter:" ,letter)
+                _str = line.split()
+                if not (len(_str)==0):
+                    arr_path.append((float)(_str[0])+i,(float)(_str[1]))
+                    print("d")
+                else:
+                    break
+        i=i+1
+        print("e")
+    print("c")
+    return arr_path
+
+# arr_path_B = []
+# with open(sys.argv[1], "r") as file_path_B:
+#     for idx, line in enumerate(file_path_B):
+#         str = line.split()
+#         if not (len(str)==0):
+#             #print(str)
+#             arr_path_B.append([(float)(str[1]), (float)(str[0])])
+#         else:
+#             break
 # now we imported arr_path_B from file to arr_path_B
 
 class GotoPoint():
     def __init__(self):
+        print("a")
         rospy.init_node('turtlebot3_sandbot', anonymous=False, disable_signals=True)
         rospy.on_shutdown(self.shutdown)
         self.cmd_vel = rospy.Publisher('/cmd_vel', Twist, queue_size=5)
@@ -69,10 +92,12 @@ class GotoPoint():
         try:
             self.tf_listener.waitForTransform(self.odom_frame, '/base_footprint', rospy.Time(), rospy.Duration(1.0))
             self.base_frame = '/base_footprint'
+            print("base_footprint")
         except (tf.Exception, tf.ConnectivityException, tf.LookupException):
             try:
                 self.tf_listener.waitForTransform(self.odom_frame, '/base_link', rospy.Time(), rospy.Duration(1.0))
                 self.base_frame = '/base_link'
+                print("base_link")
             except (tf.Exception, tf.ConnectivityException, tf.LookupException):
                 rospy.loginfo("Cannot find transform between /odom and /base_link or /base_footprint")
                 rospy.signal_shutdown("tf Exception")
@@ -87,20 +112,20 @@ class GotoPoint():
         (position, rotation) = self.get_odom()
         print("x, y, rotation", position.x, position.y, np.rad2deg(rotation))
         
-        lin_vel=0.1
+        lin_vel=0.08
         # (goal_x, goal_y, goal_z) = self.getkey()
 
         # go through path array
         ind = 1
-        length = len(arr_path_B)
-        init_goal = arr_path_B[0]
-        goal_x = arr_path_B[ind][0]-init_goal[0]
-        goal_y = arr_path_B[ind][1]-init_goal[1]
+        length = len(arr_path)
+        init_goal = arr_path[0]
+        goal_x = arr_path[ind][0]-init_goal[0]
+        goal_y = arr_path[ind][1]-init_goal[1]
 
         thres1=np.deg2rad(30)
         thres2=np.deg2rad(15)
-        ang_vel_1=0.4
-        ang_vel_2=0.15
+        ang_vel_1=0.3
+        ang_vel_2=0.1
 
         while ind < length:
             # if goal_z > 180 or goal_z < -180:
@@ -167,8 +192,8 @@ class GotoPoint():
             
             print("Now at Waypoint No.", ind)
             ind = ind + increment
-            goal_x = arr_path_B[ind][0]-init_goal[0]
-            goal_y = arr_path_B[ind][1]-init_goal[1]
+            goal_x = arr_path[ind][0]-init_goal[0]
+            goal_y = arr_path[ind][1]-init_goal[1]
 
 
             if rospy.is_shutdown():
@@ -186,6 +211,7 @@ class GotoPoint():
             rospy.loginfo("TF Exception")
             return
         pnt=Point(*trans)
+        print("point:", pnt.x, pnt.y)
         pnt.x=pnt.x-self.offset_x
         pnt.y=pnt.y-self.offset_y
 
@@ -200,11 +226,14 @@ class GotoPoint():
         self.cmd_vel.publish(Twist())
         rospy.sleep(1)
 
+
 if __name__ == '__main__':
     try:
-        while not rospy.is_shutdown():
-            #print(msg)
-            GotoPoint()
+        word=raw_input("Type a word:")
+        print("word:", word)
+        get_path(word)
+        print("path loaded")
+        GotoPoint()
 
     except:
         rospy.loginfo("shutdown program.")
