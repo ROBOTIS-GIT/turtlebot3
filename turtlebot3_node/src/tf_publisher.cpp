0569a5bc831bbeafb367e969dfebc46380decf3d
Darby Lim <thlim@robotis.com>
2018-08-27 14:09:38 +0900

    modified code format

--- turtlebot3_node/src/tf_publisher.cpp
+++ turtlebot3_node/src/tf_publisher.cpp
@@ -27,49 +27,78 @@
 
 using namespace std::chrono_literals;
 
-class TransformPublisher : public rclcpp::Node
+// class TransformPublisher : public rclcpp::Node
+// {
+// public:
+//   TransformPublisher()
+//       : Node("tf_publisher")
+//   {
+//     tf_broadcaster_ = std::make_shared<tf2_ros::TransformBroadcaster>(shared_from_this());
+
+//     odom_sub_ = this->create_subscription<nav_msgs::msg::Odometry>(
+//         "odom",
+//         [this](nav_msgs::msg::Odometry::UniquePtr msg) {
+//           this->odom_tf_transform_.transform.translation.x = msg->pose.pose.position.x;
+//           this->odom_tf_transform_.transform.translation.y = msg->pose.pose.position.y;
+//           this->odom_tf_transform_.transform.translation.z = msg->pose.pose.position.z;
+//           this->odom_tf_transform_.transform.rotation      = msg->pose.pose.orientation;
+//         });
+
+//     auto timer_callback =
+//       [this]() -> void {
+//         auto now = rclcpp::Clock().now();
+
+//         this->odom_tf_transform_.header.frame_id = "odom";
+//         this->odom_tf_transform_.child_frame_id = "base_footprint";
+//         this->odom_tf_transform_.header.stamp = now;
+
+//         this->tf_broadcaster_->sendTransform(this->odom_tf_transform_);
+//       };
+//     timer_ = this->create_wall_timer(33ms, timer_callback);
+//   }
+
+// private:
+//   rclcpp::TimerBase::SharedPtr timer_;
+
+//   rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
+
+//   std::shared_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
+//   geometry_msgs::msg::TransformStamped odom_tf_transform_;
+// };
+
+rclcpp::Node::SharedPtr node = nullptr;
+
+void odomMsgCallback(const nav_msgs::msg::Odometry::SharedPtr msg)
 {
-public:
-  TransformPublisher()
-      : Node("tf_publisher"), tf_broadcaster_(shared_from_this())
-  {
-    odom_sub_ = this->create_subscription<nav_msgs::msg::Odometry>(
-        "odom",
-        [this](nav_msgs::msg::Odometry::UniquePtr msg) {
-          this->odom_tf_transform_.transform.translation.x = msg->pose.pose.position.x;
-          this->odom_tf_transform_.transform.translation.y = msg->pose.pose.position.y;
-          this->odom_tf_transform_.transform.translation.z = msg->pose.pose.position.z;
-          this->odom_tf_transform_.transform.rotation      = msg->pose.pose.orientation;
-        });
+  tf2_ros::TransformBroadcaster tf_broadcaster(node);
+  geometry_msgs::msg::TransformStamped odom_tf_transform;
 
-    auto timer_callback =
-      [this]() -> void {
-        rclcpp::TimeSource ts(shared_from_this());
-        rclcpp::Clock::SharedPtr clock = std::make_shared<rclcpp::Clock>(RCL_ROS_TIME);
-        ts.attachClock(clock);
+  odom_tf_transform.transform.translation.x = msg->pose.pose.position.x;
+  odom_tf_transform.transform.translation.y = msg->pose.pose.position.y;
+  odom_tf_transform.transform.translation.z = msg->pose.pose.position.z;
+  odom_tf_transform.transform.rotation      = msg->pose.pose.orientation;
 
-        this->odom_tf_transform_.header.frame_id = "odom";
-        this->odom_tf_transform_.child_frame_id = "base_footprint";
-        this->odom_tf_transform_.header.stamp = clock->now();
+  auto now = rclcpp::Clock().now();
 
-        this->tf_broadcaster_.sendTransform(this->odom_tf_transform_);
-      };
-    timer_ = this->create_wall_timer(33ms, timer_callback);
-  }
+  odom_tf_transform.header.frame_id = "odom";
+  odom_tf_transform.child_frame_id = "base_footprint";
+  odom_tf_transform.header.stamp = now;
 
-private:
-  rclcpp::TimerBase::SharedPtr timer_;
-
-  rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
-
-  tf2_ros::TransformBroadcaster tf_broadcaster_;
-  geometry_msgs::msg::TransformStamped odom_tf_transform_;
-};
+  tf_broadcaster.sendTransform(odom_tf_transform);
+}
 
 int main(int argc, char *argv[])
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<TransformPublisher>());
+
+  node = rclcpp::Node::make_shared("tf_publisher");
+
+  auto odom_sub = node->create_subscription<nav_msgs::msg::Odometry>("odom", odomMsgCallback);
+
+  rclcpp::spin(node);
   rclcpp::shutdown();
+
+  odom_sub = nullptr;
+  node = nullptr;
   return 0;
 }
