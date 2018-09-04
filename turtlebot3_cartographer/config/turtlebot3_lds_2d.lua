8d3a35edad1b4e93a5595f77d8139f74a3461b2b
Darby Lim <thlim@robotis.com>
2018-09-04 14:10:13 +0900

    update cartographer config

--- turtlebot3_cartographer/config/turtlebot3_lds_2d.lua
+++ turtlebot3_cartographer/config/turtlebot3_lds_2d.lua
@@ -12,49 +12,10 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 
+-- /* Author: Darby Lim */
+
 include "map_builder.lua"
 include "trajectory_builder.lua"
-
--- options = {
---   map_builder = MAP_BUILDER,
---   trajectory_builder = TRAJECTORY_BUILDER,
---   map_frame = "map",
---   tracking_frame = "imu_link", -- imu_link, If you are using gazebo, use 'base_footprint' (libgazebo_ros_imu's bug)
---   published_frame = "odom",
---   odom_frame = "odom",
---   provide_odom_frame = false,
---   publish_frame_projected_to_2d = false,
---   use_odometry = true,
---   -- use_nav_sat = false,
---   -- use_landmarks = false,
---   num_laser_scans = 1,
---   num_multi_echo_laser_scans = 0,
---   num_subdivisions_per_laser_scan = 1,
---   num_point_clouds = 0,
---   lookup_transform_timeout_sec = 0.2,
---   submap_publish_period_sec = 0.3,
---   pose_publish_period_sec = 5e-3,
---   trajectory_publish_period_sec = 30e-3,
---   rangefinder_sampling_ratio = 1.,
---   odometry_sampling_ratio = 0.1,
---   -- fixed_frame_pose_sampling_ratio = 1.,
---   imu_sampling_ratio = 0.1,
---   -- landmarks_sampling_ratio = 1.,
--- }
-
--- MAP_BUILDER.use_trajectory_builder_2d = true
-
--- TRAJECTORY_BUILDER_2D.min_range = 0.1
--- TRAJECTORY_BUILDER_2D.max_range = 3.5
--- TRAJECTORY_BUILDER_2D.missing_data_ray_length = 3.
--- TRAJECTORY_BUILDER_2D.use_imu_data = true
--- -- TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
--- TRAJECTORY_BUILDER_2D.motion_filter.max_angle_radians = math.rad(0.1)
-
--- POSE_GRAPH.constraint_builder.min_score = 0.65
--- POSE_GRAPH.constraint_builder.global_localization_min_score = 0.7
-
--- return options
 
 options = {
   map_builder = MAP_BUILDER,
@@ -65,19 +26,19 @@ options = {
   odom_frame = "odom",
   provide_odom_frame = false,
   use_odometry = true,
-  -- use_laser_scan = true,
-  num_laser_scans = 1,
-  -- use_multi_echo_laser_scan = false,
-  num_multi_echo_laser_scans = 0,
-  num_subdivisions_per_laser_scan = 1,
+  use_laser_scan = true,
+  -- num_laser_scans = 1,
+  use_multi_echo_laser_scan = false,
+  -- num_multi_echo_laser_scans = 0,
+  -- num_subdivisions_per_laser_scan = 1.,
   num_point_clouds = 0,
   lookup_transform_timeout_sec = 0.2,
   submap_publish_period_sec = 0.3,
   pose_publish_period_sec = 5e-3,
-  trajectory_publish_period_sec = 30e-3,
-  rangefinder_sampling_ratio = 1.0,
-  odometry_sampling_ratio = 1.,
-  imu_sampling_ratio = 1.,
+  -- trajectory_publish_period_sec = 30e-3,
+  -- rangefinder_sampling_ratio = 1.,
+  -- odometry_sampling_ratio = 1.,
+  -- imu_sampling_ratio = 1.,
 }
 
 MAP_BUILDER.use_trajectory_builder_2d = true
@@ -86,16 +47,19 @@ TRAJECTORY_BUILDER_2D.min_range = 0.1
 TRAJECTORY_BUILDER_2D.max_range = 3.5
 TRAJECTORY_BUILDER_2D.missing_data_ray_length = 3.
 TRAJECTORY_BUILDER_2D.use_imu_data = false
-TRAJECTORY_BUILDER_2D.motion_filter.max_time_seconds = 0.3
-TRAJECTORY_BUILDER_2D.motion_filter.max_distance_meters = 0.2
-TRAJECTORY_BUILDER_2D.motion_filter.max_angle_radians = math.rad(3.)
+
+-- TRAJECTORY_BUILDER_2D.motion_filter.max_time_seconds = 0.3
+-- TRAJECTORY_BUILDER_2D.motion_filter.max_distance_meters = 0.2
+TRAJECTORY_BUILDER_2D.motion_filter.max_angle_radians = math.rad(0.1)
+
 TRAJECTORY_BUILDER_2D.ceres_scan_matcher.translation_weight = 70
 TRAJECTORY_BUILDER_2D.ceres_scan_matcher.rotation_weight = 300
 
-TRAJECTORY_BUILDER_2D.submaps.resolution = 0.035
-TRAJECTORY_BUILDER_2D.submaps.num_range_data = 120
-POSE_GRAPH.optimize_every_n_nodes = 120
-POSE_GRAPH.constraint_builder.min_score = 0.82
-POSE_GRAPH.constraint_builder.sampling_ratio = 1.
+-- TRAJECTORY_BUILDER_2D.submaps.resolution = 0.035
+-- TRAJECTORY_BUILDER_2D.submaps.num_range_data = 120
+
+SPARSE_POSE_GRAPH.optimize_every_n_scans = 120
+-- SPARSE_POSE_GRAPH.constraint_builder.min_score = 0.82
+-- SPARSE_POSE_GRAPH.constraint_builder.sampling_ratio = 1.
 
 return options
