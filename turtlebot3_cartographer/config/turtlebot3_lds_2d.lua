9113cb9adcf69ea3b12e4e0b03a43cef1b54b8a1
Darby Lim <thlim@robotis.com>
2018-09-05 10:40:02 +0900

    delete unused param

--- turtlebot3_cartographer/config/turtlebot3_lds_2d.lua
+++ turtlebot3_cartographer/config/turtlebot3_lds_2d.lua
@@ -27,18 +27,11 @@ options = {
   provide_odom_frame = false,
   use_odometry = true,
   use_laser_scan = true,
-  -- num_laser_scans = 1,
   use_multi_echo_laser_scan = false,
-  -- num_multi_echo_laser_scans = 0,
-  -- num_subdivisions_per_laser_scan = 1.,
   num_point_clouds = 0,
   lookup_transform_timeout_sec = 0.2,
   submap_publish_period_sec = 0.3,
   pose_publish_period_sec = 5e-3,
-  -- trajectory_publish_period_sec = 30e-3,
-  -- rangefinder_sampling_ratio = 1.,
-  -- odometry_sampling_ratio = 1.,
-  -- imu_sampling_ratio = 1.,
 }
 
 MAP_BUILDER.use_trajectory_builder_2d = true
@@ -48,18 +41,11 @@ TRAJECTORY_BUILDER_2D.max_range = 3.5
 TRAJECTORY_BUILDER_2D.missing_data_ray_length = 3.
 TRAJECTORY_BUILDER_2D.use_imu_data = false
 
--- TRAJECTORY_BUILDER_2D.motion_filter.max_time_seconds = 0.3
--- TRAJECTORY_BUILDER_2D.motion_filter.max_distance_meters = 0.2
 TRAJECTORY_BUILDER_2D.motion_filter.max_angle_radians = math.rad(0.1)
 
 TRAJECTORY_BUILDER_2D.ceres_scan_matcher.translation_weight = 70
 TRAJECTORY_BUILDER_2D.ceres_scan_matcher.rotation_weight = 300
 
--- TRAJECTORY_BUILDER_2D.submaps.resolution = 0.035
--- TRAJECTORY_BUILDER_2D.submaps.num_range_data = 120
-
 SPARSE_POSE_GRAPH.optimize_every_n_scans = 120
--- SPARSE_POSE_GRAPH.constraint_builder.min_score = 0.82
--- SPARSE_POSE_GRAPH.constraint_builder.sampling_ratio = 1.
 
 return options
