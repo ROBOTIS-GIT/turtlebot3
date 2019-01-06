b8bc4fe7c1b9eb22339dac494268d258b9d07a5f
Darby Lim <thlim@robotis.com>
2019-01-06 22:27:58 +0900

    modified tracking_frame

--- turtlebot3_cartographer/config/turtlebot3_lds_2d.lua
+++ turtlebot3_cartographer/config/turtlebot3_lds_2d.lua
@@ -21,11 +21,14 @@ options = {
   map_builder = MAP_BUILDER,
   trajectory_builder = TRAJECTORY_BUILDER,
   map_frame = "map",
-  tracking_frame = "base_footprint",
+  tracking_frame = "imu_link",
   published_frame = "odom",
   odom_frame = "odom",
   provide_odom_frame = false,
+  publish_frame_projected_to_2d = false,
   use_odometry = true,
+  use_nav_sat = false,
+  use_landmarks = false,
   num_laser_scans = 1,
   num_multi_echo_laser_scans = 0,
   num_subdivisions_per_laser_scan = 1,
@@ -36,15 +39,34 @@ options = {
   trajectory_publish_period_sec = 30e-3,
   rangefinder_sampling_ratio = 1.,
   odometry_sampling_ratio = 1.,
+  fixed_frame_pose_sampling_ratio = 1.,
   imu_sampling_ratio = 1.,
+  landmarks_sampling_ratio = 1.,
 }
 
 MAP_BUILDER.use_trajectory_builder_2d = true
 
-TRAJECTORY_BUILDER_2D.submaps.num_range_data = 35
 TRAJECTORY_BUILDER_2D.min_range = 0.1
 TRAJECTORY_BUILDER_2D.max_range = 3.5
 TRAJECTORY_BUILDER_2D.missing_data_ray_length = 3.
-TRAJECTORY_BUILDER_2D.use_imu_data = false
+TRAJECTORY_BUILDER_2D.use_imu_data = true
+TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
+TRAJECTORY_BUILDER_2D.motion_filter.max_angle_radians = math.rad(0.1)
+
+POSE_GRAPH.constraint_builder.min_score = 0.65
+POSE_GRAPH.constraint_builder.global_localization_min_score = 0.7
+
+-- TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
+-- TRAJECTORY_BUILDER_2D.use_imu_data = false
+-- TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.linear_search_window = 0.15
+-- TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.angular_search_window = math.rad(35.)
+
+-- POSE_GRAPH.optimization_problem.huber_scale = 1e2
+
+-- TRAJECTORY_BUILDER_2D.submaps.num_range_data = 35
+-- TRAJECTORY_BUILDER_2D.min_range = 0.1
+-- TRAJECTORY_BUILDER_2D.max_range = 3.5
+-- TRAJECTORY_BUILDER_2D.missing_data_ray_length = 3.
+-- TRAJECTORY_BUILDER_2D.use_imu_data = false
 
 return options
