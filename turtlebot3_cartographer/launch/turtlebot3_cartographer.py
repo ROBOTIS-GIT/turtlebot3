441b6c06868084e2efde995c5d9e0c07d261600e
Darby Lim <thlim@robotis.com>
2019-01-15 16:07:29 +0900

    delete exit

--- turtlebot3_cartographer/launch/turtlebot3_cartographer.py
+++ turtlebot3_cartographer/launch/turtlebot3_cartographer.py
@@ -17,7 +17,6 @@
 import os
 
 from ament_index_python.packages import get_package_share_directory
-from launch.exit_handler import restart_exit_handler
 from ros2run.api import get_executable_path
 
 def launch(launch_descriptor, argv):
@@ -38,7 +37,6 @@ def launch(launch_descriptor, argv):
             '-configuration_basename', 'turtlebot3_lds_2d.lua'
         ],
         name='cartographer_node',
-        exit_handler=restart_exit_handler,
     )
 
     return ld
