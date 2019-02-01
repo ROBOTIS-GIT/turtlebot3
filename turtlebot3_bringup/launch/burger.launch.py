b9f7ba3b714db209747469b5a3f0bb4da75cb374
Darby Lim <thlim@robotis.com>
2019-02-01 17:07:47 +0900

    import library

--- turtlebot3_bringup/launch/burger.launch.py
+++ turtlebot3_bringup/launch/burger.launch.py
@@ -20,6 +20,7 @@ import os
 
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
+from launch.substitutions import LaunchConfiguration
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import ThisLaunchFileDir
 from launch_ros.actions import Node
