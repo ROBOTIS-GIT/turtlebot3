805a87b336f5f796a466083838f32184d8127150
Darby Lim <thlim@robotis.com>
2019-02-19 21:27:08 +0900

    delete unused var

--- turtlebot3_node/src/constants.cpp
+++ turtlebot3_node/src/constants.cpp
@@ -16,22 +16,4 @@
 
 /* Author: Darby Lim */
 
-#include <chrono>
-
 #include "constants.h"
-
-using namespace std::chrono_literals;
-
-constexpr char SensorStateTopic[] = "sensor_state";
-constexpr char JointStateTopic[] = "joint_states";
-constexpr char OdomTopic[] = "odom";
-constexpr char ScanHalfTopic[] = "scan_half";
-constexpr char ScanTopic[] = "scan";
-constexpr char ImuTopic[] = "imu";
-constexpr char TimeTopic[] = "time_sync";
-
-constexpr auto JointStatePublishPeriodMillis = 33ms;
-constexpr auto ScanPublishPeriodMillis = 33ms;
-constexpr auto OdometryPublishPeroidMillis = 33ms;
-
-constexpr double WheelRadius = 0.033;
