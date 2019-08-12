15908ac386adef8fc28a247598664635e59c5dc2
Darby Lim <thlim@robotis.com>
2019-08-12 19:24:24 +0900

    Applied ament_flake8

--- turtlebot3_bringup/launch/robot.launch.py
+++ turtlebot3_bringup/launch/robot.launch.py
@@ -16,27 +16,27 @@
 
 # /* Author: Darby Lim */
 
-import os
-
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
-from launch.substitutions import LaunchConfiguration
 from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
 from launch.substitutions import ThisLaunchFileDir
 from launch_ros.actions import Node
+
 
 def generate_launch_description():
     use_sim_time = LaunchConfiguration('use_sim_time', default='false')
 
     return LaunchDescription([
         DeclareLaunchArgument(
-           'use_sim_time', 
+           'use_sim_time',
            default_value=use_sim_time,
            description='Use simulation (Gazebo) clock if true'),
 
         IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([ThisLaunchFileDir(), '/turtlebot3_state_publisher.launch.py']),
+            PythonLaunchDescriptionSource(
+                [ThisLaunchFileDir(), '/turtlebot3_state_publisher.launch.py']),
             launch_arguments={'use_sim_time': use_sim_time}.items(),
         ),
 
--- turtlebot3_bringup/launch/rviz2.launch.py
+++ turtlebot3_bringup/launch/rviz2.launch.py
@@ -22,11 +22,14 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
-def generate_launch_description():
-    rviz_config_dir = os.path.join(get_package_share_directory('turtlebot3_description'), 'rviz', 'model.rviz')
-    
-    return LaunchDescription([
 
+def generate_launch_description():
+    rviz_config_dir = os.path.join(
+            get_package_share_directory('turtlebot3_description'),
+            'rviz',
+            'model.rviz')
+
+    return LaunchDescription([
         Node(
             package='rviz2',
             node_executable='rviz2',
