f2209819bb8109cd77f15b903690e12355322732
Ryan Shim <jhshim@robotis.com>
2020-06-12 12:02:25 +0900

    updated launch file in tb3_nav2

--- turtlebot3_navigation2/launch/navigation2.launch.py
+++ turtlebot3_navigation2/launch/navigation2.launch.py
@@ -67,7 +67,7 @@ def generate_launch_description():
             description='Use simulation (Gazebo) clock if true'),
 
         IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([nav2_launch_file_dir, '/nav2_bringup_launch.py']),
+            PythonLaunchDescriptionSource([nav2_launch_file_dir, '/bringup_launch.py']),
             launch_arguments={
                 'map': map_dir,
                 'use_sim_time': use_sim_time,
@@ -76,8 +76,8 @@ def generate_launch_description():
 
         Node(
             package='rviz2',
-            node_executable='rviz2',
-            node_name='rviz2',
+            executable='rviz2',
+            name='rviz2',
             arguments=['-d', rviz_config_dir],
             parameters=[{'use_sim_time': use_sim_time}],
             output='screen'),
