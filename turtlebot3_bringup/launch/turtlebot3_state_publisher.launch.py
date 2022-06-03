cb8b090982ff6d45ab3048e1848efa70c867750e
Will Son <willson@robotis.com>
2022-06-03 17:34:17 +0900

    revert lint ci
    Signed-off-by: Will Son <willson@robotis.com>

--- turtlebot3_bringup/launch/turtlebot3_state_publisher.launch.py
+++ turtlebot3_bringup/launch/turtlebot3_state_publisher.launch.py
@@ -23,7 +23,6 @@ from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
-from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
