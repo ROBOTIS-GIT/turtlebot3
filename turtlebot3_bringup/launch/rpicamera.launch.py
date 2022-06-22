274836b33e49b2b289ebc4e721666debed583a50
Nathan Sprague <nathan.r.sprague@gmail.com>
2022-06-22 18:25:23 +0000

    Add launch file for pi camera

---
+++ turtlebot3_bringup/launch/rpicamera.launch.py
@@ -0,0 +1,34 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch.substitutions import ThisLaunchFileDir
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    camera_params = LaunchConfiguration(
+        'camera_params',
+        default=os.path.join(
+            get_package_share_directory('turtlebot3_bringup'),
+            'param',
+            'camera.yaml'))
+
+    return LaunchDescription([
+
+        Node(
+            package='usb_cam',
+            executable='usb_cam_node_exe',
+            parameters=[camera_params],
+            arguments=['--ros-args', '--remap', '__ns:=/camera'],
+            remappings=[
+                ('/camera/image_raw', '/camera/image_raw/uncompressed'),
+            ],
+            output='screen'
+            ),
+    ])
