a3163f3304905c0a91b851ed38e6c799bb8d1836
Nathan Sprague <Nathan Sprague>
2022-06-27 13:17:39 -0400

    add dependencies and remove some unused imports

--- turtlebot3_bringup/launch/rpicamera.launch.py
+++ turtlebot3_bringup/launch/rpicamera.launch.py
@@ -3,11 +3,7 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.actions import IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
-from launch.substitutions import ThisLaunchFileDir
 from launch_ros.actions import Node
 
 
