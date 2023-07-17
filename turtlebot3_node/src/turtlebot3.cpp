f73fb2ec97ef850e76e4114e3cb09d550a6613d9
坂巻翔太 <80994423+ymkkk@users.noreply.github.com>
2023-07-17 15:50:39 +0900

    Update turtlebot3.cpp
    debug comment out

--- turtlebot3_node/src/turtlebot3.cpp
+++ turtlebot3_node/src/turtlebot3.cpp
@@ -356,15 +356,16 @@ void TurtleBot3::cmd_vel_callback()
 
       float x_lim = 0.0;
       float y_lim = 0.0;
-      float linear_x = static_cast<float>(msg->linear.x * 1.0);
-      float angular_z = static_cast<float>(msg->angular.z * 1.0);
+      //float linear_x = static_cast<float>(msg->linear.x * 1.0);
+      //float angular_z = static_cast<float>(msg->angular.z * 1.0);
 
       if(linear_x > 1.0){
         x_lim = 1.0;
       }else if(linear_x < -1.0){
         x_lim = -1.0;
       }else{
-        x_lim = linear_x * 5; // 0.1から0.5にする
+        x_lim=0.5
+        //x_lim = linear_x * 5; // 0.1から0.5にする
       }
 
       if(angular_z > 1.0){
@@ -372,7 +373,8 @@ void TurtleBot3::cmd_vel_callback()
       }else if(angular_z < -1.0){
         y_lim = -1.0;
       }else{
-        y_lim = angular_z;
+        y_lim=0.5
+        //y_lim = angular_z;
       }
       
 
