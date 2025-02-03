d81e95aae6986c1df10ed7df1ff8e2cefb3da5fc
GyuH13 <gyu@robotis.com>
2025-02-03 16:26:03 +0900

    Add namespace argument for multi robot control in turtlebot3 bringup launch

--- turtlebot3_bringup/launch/turtlebot3_state_publisher.launch.py
+++ turtlebot3_bringup/launch/turtlebot3_state_publisher.launch.py
@@ -21,12 +21,14 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration, PythonExpression
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
     TURTLEBOT3_MODEL = os.environ['TURTLEBOT3_MODEL']
+
+    namespace = LaunchConfiguration('namespace')
 
     use_sim_time = LaunchConfiguration('use_sim_time', default='false')
     urdf_file_name = 'turtlebot3_' + TURTLEBOT3_MODEL + '.urdf'
@@ -56,5 +58,8 @@ def generate_launch_description():
             package='robot_state_publisher',
             executable='robot_state_publisher',
             output='screen',
-            parameters=[rsp_params, {'use_sim_time': use_sim_time}])
+            parameters=[
+                    rsp_params,
+                    {'use_sim_time': use_sim_time},
+                    {'frame_prefix': PythonExpression(["'", namespace, "/'"])}])
     ])
