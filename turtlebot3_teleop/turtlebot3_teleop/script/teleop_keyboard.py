d55c737005f750720d0f66b466ccfe57cd034ba5
Hyungyu Kim <kimhg@robotis.com>
2025-03-25 07:38:18 +0000

    Changed timestamped
    Signed-off-by: Hyungyu Kim <kimhg@robotis.com>

--- turtlebot3_teleop/turtlebot3_teleop/script/teleop_keyboard.py
+++ turtlebot3_teleop/turtlebot3_teleop/script/teleop_keyboard.py
@@ -41,6 +41,7 @@ import sys
 from geometry_msgs.msg import Twist, TwistStamped
 import rclpy
 from rclpy.qos import QoSProfile
+from rclpy.clock import Clock
 
 if os.name == 'nt':
     import msvcrt
@@ -217,7 +218,7 @@ def main():
                 pub.publish(twist)
             else:
                 twist_stamped = TwistStamped()
-                twist_stamped.header.stamp = rclpy.Time.now()
+                twist_stamped.header.stamp = Clock().now().to_msg()
                 twist_stamped.header.frame_id = ''
                 twist_stamped.twist.linear.x = control_linear_velocity
                 twist_stamped.twist.linear.y = 0.0
@@ -244,7 +245,7 @@ def main():
             pub.publish(twist)
         else:
             twist_stamped = TwistStamped()
-            twist_stamped.header.stamp = rclpy.Time.now()
+            twist_stamped.header.stamp = now()
             twist_stamped.header.frame_id = ''
             twist_stamped.twist.linear.x = control_linear_velocity
             twist_stamped.twist.linear.y = 0.0
