d9466b94dbb21bb8340086fa70c1e45f9e234339
Hyungyu Kim <kimhg@robotis.com>
2025-03-25 16:06:33 +0900

    teleop update
    Signed-off-by: Hyungyu Kim <kimhg@robotis.com>

--- turtlebot3_teleop/turtlebot3_teleop/script/teleop_keyboard.py
+++ turtlebot3_teleop/turtlebot3_teleop/script/teleop_keyboard.py
@@ -38,7 +38,7 @@ import os
 import select
 import sys
 
-from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Twist, TwistStamped
 import rclpy
 from rclpy.qos import QoSProfile
 
@@ -142,10 +142,13 @@ def main():
         settings = termios.tcgetattr(sys.stdin)
 
     rclpy.init()
-
+    ROS_DISTRO = os.environ.get('ROS_DISTRO')
     qos = QoSProfile(depth=10)
     node = rclpy.create_node('teleop_keyboard')
-    pub = node.create_publisher(Twist, 'cmd_vel', qos)
+    if ROS_DISTRO == 'humble':
+        pub = node.create_publisher(Twist, 'cmd_vel', qos)
+    else:
+        pub = node.create_publisher(TwistStamped, 'cmd_vel', qos)
 
     status = 0
     target_linear_velocity = 0.0
@@ -191,42 +194,69 @@ def main():
                 print(msg)
                 status = 0
 
-            twist = Twist()
-
             control_linear_velocity = make_simple_profile(
                 control_linear_velocity,
                 target_linear_velocity,
                 (LIN_VEL_STEP_SIZE / 2.0))
-
-            twist.linear.x = control_linear_velocity
-            twist.linear.y = 0.0
-            twist.linear.z = 0.0
 
             control_angular_velocity = make_simple_profile(
                 control_angular_velocity,
                 target_angular_velocity,
                 (ANG_VEL_STEP_SIZE / 2.0))
 
-            twist.angular.x = 0.0
-            twist.angular.y = 0.0
-            twist.angular.z = control_angular_velocity
+            if ROS_DISTRO == 'humble':
+                twist = Twist()
+                twist.linear.x = control_linear_velocity
+                twist.linear.y = 0.0
+                twist.linear.z = 0.0
 
-            pub.publish(twist)
+                twist.angular.x = 0.0
+                twist.angular.y = 0.0
+                twist.angular.z = control_angular_velocity
+
+                pub.publish(twist)
+            else:
+                twist_stamped = TwistStamped()
+                twist_stamped.header.stamp = rclpy.Time.now()
+                twist_stamped.header.frame_id = ""
+                twist_stamped.twist.linear.x = control_linear_velocity
+                twist_stamped.twist.linear.y = 0.0
+                twist_stamped.twist.linear.z = 0.0
+
+                twist_stamped.twist.angular.x = 0.0
+                twist_stamped.twist.angular.y = 0.0
+                twist_stamped.twist.angular.z = control_angular_velocity
+
+                pub.publish(twist_stamped)
+
+
+
+
 
     except Exception as e:
         print(e)
 
     finally:
-        twist = Twist()
-        twist.linear.x = 0.0
-        twist.linear.y = 0.0
-        twist.linear.z = 0.0
-
-        twist.angular.x = 0.0
-        twist.angular.y = 0.0
-        twist.angular.z = 0.0
-
-        pub.publish(twist)
+        if ROS_DISTRO == 'humble':
+            twist = Twist()
+            twist.linear.x = 0.0
+            twist.linear.y = 0.0
+            twist.linear.z = 0.0
+            twist.angular.x = 0.0
+            twist.angular.y = 0.0
+            twist.angular.z = 0.0
+            pub.publish(twist)
+        else:
+            twist_stamped = TwistStamped()
+            twist_stamped.header.stamp = rclpy.Time.now()
+            twist_stamped.header.frame_id = ""
+            twist_stamped.twist.linear.x = control_linear_velocity
+            twist_stamped.twist.linear.y = 0.0
+            twist_stamped.twist.linear.z = 0.0
+            twist_stamped.twist.angular.x = 0.0
+            twist_stamped.twist.angular.y = 0.0
+            twist_stamped.twist.angular.z = control_angular_velocity
+            pub.publish(twist_stamped)
 
         if os.name != 'nt':
             termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
