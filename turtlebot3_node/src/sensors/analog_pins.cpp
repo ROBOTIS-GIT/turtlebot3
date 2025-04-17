d0402266090433a753e5ad7be19c2995b62cc438
travis-mendoza <travis.mendoza@eyezense.com>
2025-04-16 21:37:56 -0700

    assign copyright of analog pins files to ROBOTIS

--- turtlebot3_node/src/sensors/analog_pins.cpp
+++ turtlebot3_node/src/sensors/analog_pins.cpp
@@ -1,4 +1,4 @@
-// Copyright 2025 Travis Mendoza
+// Copyright 2025 ROBOTIS CO., LTD.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
